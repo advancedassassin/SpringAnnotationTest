287653783dad8610f76f1beb4fa45d6e614eff45
= <932938014@qq.com>
2020-09-15 14:11:19 +0800

    GitEmergeTest

---
+++ src/main/java/com/ydx/gittest/GitEmergeTest.java
@@ -0,0 +1,25 @@
+package com.ydx.gittest;
+
+/**
+ * @ClassName GitEmergeTest
+ * @Description 描述一下该类用途
+ * @Author Duxin Yuan
+ * @Date 2020/9/15 14:08
+ * @Version 1.0
+ **/
+public class GitEmergeTest {
+
+    public void gitEmergeTest(){
+        System.out.println("需要删除的错误代码");
+
+        System.out.println("需要修改的代码---（）");
+
+
+        // 不变的代码
+        for (int i = 1;i<=10;i++) {
+            System.out.println(i);
+        }
+
+    }
+
+}
